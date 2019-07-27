12598e2b459f21b50e14bae768a11b4a8a1cf7f4
Juliana Fogg <julianafogg@Julianas-MBP.fios-router.home>
2019-07-27 19:59:41 -0400

    service-worker

---
+++ build/precache-manifest.cfd5e0d86c106f0eac793ca329b7679f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d513e464a230761defd28386c27bd7a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87c0a32f662c7f73bc9d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dca651c1692a08e448f1",
+    "url": "/static/js/2.7f43b054.chunk.js"
+  },
+  {
+    "revision": "87c0a32f662c7f73bc9d",
+    "url": "/static/js/main.4573318c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
