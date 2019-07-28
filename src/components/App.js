59f9c11c82414fff666a1162a916e078d10c280c
Alex Guevara <aguevara@movableink.com>
2019-07-27 18:29:41 -0700

    logs entries from missing people and found people collections for easier debugging

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import CheckIn from './CheckIn';
 import Search from './Search';
 import AssignLocation from './AssignLocation';
 import { loginAnonymous } from './../stitch/auth';
+import { stitchClient, db } from '../stitch/database';
 
 export function Home() {
   return(
@@ -16,8 +17,18 @@ export function Home() {
 }
 
 class App extends Component {
-  componentDidMount() {
+  async componentDidMount() {
     loginAnonymous();
+
+    // NOTE: this is just for testing so you can easily see the people in each collection
+    this.client = stitchClient;
+    this.db = db;
+
+    const missingPeople = await this.db.collection('missing').find().toArray();
+    const foundPeople = await this.db.collection('found').find().toArray();
+    console.log('MISSING PEOPLE: ', missingPeople);
+    console.log('FOUND PEOPLE: ', foundPeople);
+
   }
   render() {
     return(
